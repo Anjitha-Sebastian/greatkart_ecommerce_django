2e73a836936989691b8792f8392371f5d04c4738
Anjitha-Sebastian <anjithasebastian@gmail.com>
2024-04-22 16:28:06 +0530

    store and carts functionality

--- store/urls.py
+++ store/urls.py
@@ -4,6 +4,7 @@ from .import views
 
 urlpatterns = [
     path('',views.storeview,name='store'),
-    path('<slug:category_slug>/', views.storeview, name='products_by_category'),
-    path('<slug:category_slug>/<slug:product_slug>', views.product_detail, name='products_detail')
+    path('category/<slug:category_slug>/', views.storeview, name='products_by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>', views.product_detail, name='products_detail'),
+    path('search/',views.search,name='search')
 ]
